33dca49cb247f3db4b534b4d4f69ae9b249f6997
matheusicaro <matheusicaro2@hotmail.com>
2021-12-24 18:16:57 -0300

    Starting Creat React App with TypeScript template .

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-import Routes from './routes';
-import store from './store';
-
-const App: React.FC = () => {
+function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,9 +1,1 @@
-declare module '*.png' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.svg' {
-  const content: string;
-  export default content;
-}
+/// <reference types="react-scripts" />
